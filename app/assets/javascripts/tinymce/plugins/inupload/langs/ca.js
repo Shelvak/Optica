412c4b9e84c57c8f0712acda8fcecb13accaca89
Néstor Coppi <nestorcoppi@gmail.com>
2011-11-29 15:34:56 -0300

    a produccion

--- app/assets/javascripts/tinymce/plugins/inupload/langs/ca.js
+++
@@ -1,9 +0,0 @@
-tinyMCE.addI18n('ca.inupload',{
-	desc : 'Opció per inserir imatges i documents...',
-  opcions: '2. Tria les opcions (optatives) ...',
-  linkn: 'Text del enllaç (només documents)',
-  minw: 'Amplada miniatura (només imatges)',
-  maxw: 'Amplada imatge gran (només imatges)',
-  title: 'Inserir imatges i documents',
-  fitxer:'1. Sel.lecciona el fitxer que vulguis inserir ....'  
-});
