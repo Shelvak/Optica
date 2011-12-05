64b9aac502881ff2e64f7a6f33237d59de5c18e7
Néstor Coppi <nestorcoppi@gmail.com>
2011-12-05 13:39:13 -0300

    Agregada busqueda cliente

--- app/helpers/clientes_helper.rb
+++ app/helpers/clientes_helper.rb
@@ -5,5 +5,18 @@ module ClientesHelper
     return (Date.today.year - @cump.nacimiento.year.to_i)
   end
   
+  def edad_show(cump)
+    @cump = cump
+    if Date.today.month <= @cump.nacimiento.month.to_i && 
+        Date.today.day < @cump.nacimiento.day.to_i
+      return (Date.today.year - @cump.nacimiento.year.to_i - 1)
+    elsif Date.today.month == @cump.nacimiento.month.to_i && 
+        Date.today.day == @cump.nacimiento.day.to_i
+      return (Date.today.year - @cump.nacimiento.year.to_i)
+    elsif Date.today.month >= @cump.nacimiento.month.to_i && 
+        Date.today.day > @cump.nacimiento.day.to_i
+      return (Date.today.year - @cump.nacimiento.year.to_i - 1)
+    end
+  end
   
 end
