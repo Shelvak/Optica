284e364eabaf6dcbc40f974c19fdd5acde005fd4
Néstor Coppi <nestorcoppi@gmail.com>
2011-12-13 12:54:07 -0300

    Arreglos menores

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
   acts_as_authentic
+  
+  before_save :asignar_admin
+  
+  def asignar_admin
+    @a = User.all
+    self.admin = true unless @a.present?
+  end
+  
 end
