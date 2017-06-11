a2d0441c4a7cc80823a38dd9e40d6ac866ab68ff
Néstor Coppi <nestorcoppi@gmail.com>
2017-06-11 13:43:30 -0300

    Arrancando con snoopy

---
+++ app/models/bill.rb
@@ -0,0 +1,25 @@
+class Bill
+  require 'snoopy_afip'
+  Snoopy.auth_url    = "https://wsaa.afip.gov.ar/ws/services/LoginCms"
+  Snoopy.service_url =  './prod.wsdl' #lib_path + '/files/prod.wsdl'
+  Snoopy.default_moneda    = :peso
+  Snoopy.default_concepto  = 'Servicios'
+  Snoopy.default_documento = 'CUIT'
+
+  data = {}
+  data[:concepto] = 'Productos y Servicios'
+  data[:imp_iva] = 0.21
+  data[:alicivas] = [{id: 0.21,  importe: 1,  base_imp: 0.21}]
+  data[:net] = 0.21
+  data[:doc_num]  = '34675601'
+  data[:documento] = 'DNI'
+  data[:fch_serv_desde] = '20170610'
+  data[:fch_serv_hasta] = '20170620'
+  data[:iva_cond] = :factura_b
+  def self.send_milonga(attrs)
+    data = { pkey: './NUEVA-optica-private.key', cert: './NUEVO-optica.crt', cuit: '27125786524', sale_point: '0004', own_iva_cond: :responsable_inscripto }
+    bill = Snoopy::Bill.new(data.merge(attrs))
+    bill.cae_request
+    bill
+  end
+end
